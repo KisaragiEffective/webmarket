9708a8af08ef049821c13b14885b66e279bbe310
KisaragiEffective <kisaragi.effective@gmail.com>
2020-07-17 00:10:06 +0900

    update migration

--- database/migrations/2020_07_12_154510_create_necessary_tables.php
+++ database/migrations/2020_07_12_154510_create_necessary_tables.php
@@ -13,6 +13,7 @@ class CreateNecessaryTables extends Migration
      */
     public function up()
     {
+        // userテーブル
         Schema::create(DatabaseGateway::TABLE_USER, function (Blueprint $table) {
             $table->charset = 'utf8mb4';
             $table->bigIncrements('id');
@@ -23,16 +24,82 @@ class CreateNecessaryTables extends Migration
             $table->unique('uuid');
         });
         
+        Schema::create("shop_permission", function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->integer('user_id');
+            $table->integer('shop_id');
+            $table->boolean('update')->default(false);
+            $table->boolean('new')->default(false);
+            $table->boolean('delete')->default(false);
+        });
+        
+        Schema::create("nickname", function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->integer('user_id');
+            $table->string('nickname');
+            $table->unique('nickname');
+        });
+        
+        // shopテーブル
         Schema::create(DatabaseGateway::TABLE_SHOP, function (Blueprint $table) {
             $table->charset = 'utf8mb4';
             $table->bigIncrements('id');
             $table->string('name', 255);
-            $table->integer('owner');
             // ダブリは紛らわしいから禁止！
             $table->unique('name');
             // shops.owner <-> users.id
             // $table->foreign('owner')->references('id')->on('users');
         });
+        
+        Schema::create("transactions", function (Blueprint $table) {
+            $table->id();
+            $table->integer("chain_id");
+            $table->integer('user_id');
+            $table->integer('quantity');
+        });
+        
+        Schema::create("transactions_chain", function(Blueprint $table){
+            $table->id();
+            $table->integer('reason');
+            $table->date('date');
+        });
+        
+        Schema::create('transactions_reason', function(Blueprint $table){
+            $table->id();
+            $table->string('stringify', 512);
+        });
+        
+        Schema::create('auction_bid', function(Blueprint $table){
+            $table->id();
+            $table->integer('chain_id');
+            $table->integer('user_id');
+            $table->integer('item_id');
+            $table->integer('quantity');
+            $table->string('stringify', 512);
+        });
+        
+        Schema::create('auction_chain', function(Blueprint $table){
+            $table->id();
+            $table->integer('reason');
+            $table->date('date');
+        });
+        
+        Schema::create('auction_reason', function(Blueprint $table){
+            $table->id();
+            $table->string('stringify', 512);
+        });
+        
+        Schema::create('bank', function(Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('item_id');
+            $table->integer('quantity');
+            $table->primary(['user_id', 'item_id']);
+        });
+        
+        Schema::create('item', function(Blueprint $table)) {
+            $table->id();
+            $table->string('stringify', 4096);
+        }
     }
 
     /**
@@ -42,6 +109,8 @@ class CreateNecessaryTables extends Migration
      */
     public function down()
     {
+        $x = [DatabaseGateway::TABLE_USER, DatabaseGateway::TABLE_SHOP];
+        
         Schema::dropIfExists(DatabaseGateway::TABLE_USER);
         Schema::dropIfExists(DatabaseGateway::TABLE_SHOP);
     }
