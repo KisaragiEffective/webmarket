d62398618d8c216b9d2cdce08086d779f1dde8f0
KisaragiEffective <kisaragi.effective@gmail.com>
2020-07-19 12:34:52 +0900

    $?を伝播させる

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ find config/ -type f | xargs grep storage_path \
 | grep framework | sed -e 's/^.*storage_path//' \
 | tr -d ",\(\)'" | awk '{ print "storage/"$0 }' | xargs mkdir -p
 
-(composer install && composer update && php artisan migrate) && echo "Setup success!" || echo "Setup failed. Exit code: $?"
+(composer install && composer update && php artisan migrate) && echo "Setup success!" || (echo "Setup failed. Exit code: $?" && exit $?)
 
