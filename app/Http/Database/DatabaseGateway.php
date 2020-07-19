d09aead324ec1add87880185bc7a2fe3ab2541bb
KisaragiEffective <kisaragi.effective@gmail.com>
2020-07-19 13:29:55 +0900

    fix syntax error in gateway file

--- app/Http/Database/DatabaseGateway.php
+++ app/Http/Database/DatabaseGateway.php
@@ -10,28 +10,31 @@ class DatabaseGateway {
     const TABLE_USER = "users";
     const TABLE_SHOP = "shops";
     /**
-    *
+    * ユーザーを返却！圧倒的成長！
     */
-    public static function getUser(string uuid): object/*: stdClass */ {
+    public static function getUser(string $uuid): object/*: stdClass */ {
         return DB::select("SELECT * FROM " . TABLE_USER . " WHERE uuid = ?", [$uuid]);
     }
-    
-    public static function registerUser(string uuid, string ign) {
+
+    /**
+    * ユーザーを登録！圧倒的成長！
+    */
+    public static function registerUser(string $uuid, string $ign) {
         DB::insert(
         "INSERT INTO " . TABLE_USER ." VALUES (?, ?, ?)",
         [0, $uuid, $ign]
         );
     }
     
-    public static function existsUser(string uuid): boolean {
+    public static function existsUser(string $uuid): boolean {
         return DB::select("SELECT COUNT(*) as count FROM" . TABLE_USER . " WHERE uuid = ?", [$uuid]) ->count !== 0;
     }
     
-    public static function updateUserName(string uuid, string newName) {
+    public static function updateUserName(string $uuid, string $newName) {
         // TODO: UPDATE PLAYER'S NAME
     }
     
-    public static function select(sql: string, parameter: array = []): int {
-        return DB::select(sql, parameter);
+    public static function select(string $sql, array $parameter = []): int {
+        return DB::select($sql, $parameter);
     }
 }
